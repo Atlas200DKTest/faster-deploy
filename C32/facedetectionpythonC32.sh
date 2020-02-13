2e5b68b292c2d345c7e945b7fc0456e9505c6700
HuaweiAscend <ascend@huawei.com>
2020-02-13 22:03:50 +0800

    update C32/facedetectionpythonC32.sh.

--- C32/facedetectionpythonC32.sh
+++ C32/facedetectionpythonC32.sh
@@ -31,7 +31,7 @@ main()
         return 1
     fi
 
-    if [ ! -f "$HOME/AscendProjects/sample-facedetection-python/script/face_detction.om" ];then
+    if [ ! -f "$HOME/AscendProjects/sample-facedetection-python/MyModel/face_detction.om" ];then
 	model_shape=`head -1 $HOME/AscendProjects/sample-facedetection-python/MyModel/shape_face_detection`
         download_url_caffemodel="https://obs-model-ascend.obs.cn-east-2.myhuaweicloud.com/face_detection/face_detection.caffemodel"
         download_url_prototxt="https://obs-model-ascend.obs.cn-east-2.myhuaweicloud.com/face_detection/face_detection.prototxt"
@@ -39,7 +39,7 @@ main()
         wget -O $HOME/AscendProjects/sample-facedetection-python/MyModel/face_detection.caffemodel ${download_url_caffemodel} --no-check-certificate
         wget -O $HOME/AscendProjects/sample-facedetection-python/MyModel/face_detection.prototxt ${download_url_prototxt} --no-check-certificate
         wget -O $HOME/AscendProjects/sample-facedetection-python/MyModel/LICENSE ${download_url_license} --no-check-certificate
-        export SLOG_PRINT_TO_STDOUT=1 && export PATH=${PATH}:${DDK_HOME}/uihost/toolchains/ccec-linux/bin/ && export LD_LIBRARY_PATH=${DDK_HOME}/uihost/lib/ && export TVM_AICPU_LIBRARY_PATH=${DDK_HOME}/uihost/lib/:${DDK_HOME}/uihost/toolchains/ccec-linux/aicpu_lib && export TVM_AICPU_INCLUDE_PATH=${DDK_HOME}/include/inc/tensor_engine && export PYTHONPATH=${DDK_HOME}/site-packages && export TVM_AICPU_OS_SYSROOT=/usr/aarch64-linux-gnu && ${DDK_HOME}/uihost/bin/omg --output="$HOME/AscendProjects/sample-facedetection-python/script/face_detection" --check_report=$HOME/AscendProjects/sample-facedetection-python/MyModel/face_detection_result.json --plugin_path= --model="$HOME/AscendProjects/sample-facedetection-python/MyModel/face_detection.prototxt" --framework=0 --ddk_version=${tools_version} --weight="$HOME/AscendProjects/sample-facedetection-python/MyModel/face_detection.caffemodel" --input_shape=${model_shape} --insert_op_conf=$HOME/AscendProjects/sample-facedetection-python/MyModel/aipp_face_detection.cfg --op_name_map=$HOME/AscendProjects/sample-facedetection-python/MyModel/reassign_operators
+        export SLOG_PRINT_TO_STDOUT=1 && export PATH=${PATH}:${DDK_HOME}/uihost/toolchains/ccec-linux/bin/ && export LD_LIBRARY_PATH=${DDK_HOME}/uihost/lib/ && export TVM_AICPU_LIBRARY_PATH=${DDK_HOME}/uihost/lib/:${DDK_HOME}/uihost/toolchains/ccec-linux/aicpu_lib && export TVM_AICPU_INCLUDE_PATH=${DDK_HOME}/include/inc/tensor_engine && export PYTHONPATH=${DDK_HOME}/site-packages && export TVM_AICPU_OS_SYSROOT=/usr/aarch64-linux-gnu && ${DDK_HOME}/uihost/bin/omg --output="$HOME/AscendProjects/sample-facedetection-python/MyModel/face_detection" --check_report=$HOME/AscendProjects/sample-facedetection-python/MyModel/face_detection_result.json --plugin_path= --model="$HOME/AscendProjects/sample-facedetection-python/MyModel/face_detection.prototxt" --framework=0 --ddk_version=${tools_version} --weight="$HOME/AscendProjects/sample-facedetection-python/MyModel/face_detection.caffemodel" --input_shape=${model_shape} --insert_op_conf=$HOME/AscendProjects/sample-facedetection-python/MyModel/aipp_face_detection.cfg --op_name_map=$HOME/AscendProjects/sample-facedetection-python/MyModel/reassign_operators
         if [ $? -ne 0 ];then
             echo "ERROR: download failed, please check network connetction."
             return 1
